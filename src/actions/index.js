d84d53c8943ebd183413adea8fe8af2307bd170e
DESKTOP-U4VHMDR\Optimax Global <naykeng1kz1@gmail.com>
2017-08-28 17:36:57 +0700

    Section Intermediate Redux: Middleware -> 58. Redux-promise in Practice is done Weather application

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'b9b9a535c045b1bec8dc470ad8aeb87e';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+//api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
